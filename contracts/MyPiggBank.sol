6bf9502ac5269936c83ac792785e672304542bdf
kusajin <yuanbinbin29@163.com>
2025-10-16 17:26:12 +0800

    solidity

--- contracts/MyPiggBank.sol
+++ contracts/MyPiggBank.sol
@@ -8,7 +8,7 @@ contract MyPiggyBank {
 
     // check DepositRatio range (0-100)
     modifier checkDepositRatio(uint256 _depositRatio) {
-        _depositRatio = _depositRatio/(10**18);
+        _depositRatio = _depositRatio / (10 ** 18);
         require(
             _depositRatio >= 0 || _depositRatio <= 100,
             "depositRatio must be between 0 and 100 "
@@ -21,6 +21,11 @@ contract MyPiggyBank {
         _;
     }
 
+    modifier checkwithraw() {
+        require(!withdrawStatus, " withdraw not finish please wait ");
+        _;
+    }
+
     //amount of cash which can be withdrawed any time
     uint256 public cash;
 
@@ -28,31 +33,30 @@ contract MyPiggyBank {
     address owner;
 
     struct depositReceipt {
-        uint256 depositTime ;
-        uint256 depositAmount ;
+        uint256 depositTime;
+        uint256 depositAmount;
     }
 
     mapping(uint256 => depositReceipt) public depositReceipts;
 
-    uint256 public lastDepositTime ;
-
-    uint256 public depositTotalAmount;
-    
     uint256 public depositTimes = 0;
+
+    uint256 public totalAmount;
 
     //deposited amount freezed seconds, 1 minutes for tutorial purpose
     uint256 constant freezeTime = 1 minutes;
 
     uint256 public depositRatio;
 
-     constructor() {
+    bool withdrawStatus = false;
+    constructor() {
         owner = msg.sender;
     }
 
     function modifyDepositRatio(
         uint256 _depositRatio
     ) external checkDepositRatio(_depositRatio) onlyOwner {
-        depositRatio = _depositRatio;        
+        depositRatio = _depositRatio;
     }
 
     //deposit method  require eth >0
@@ -62,11 +66,12 @@ contract MyPiggyBank {
         if (depositRatio == 0) {
             _depoistAmount = 0;
         } else {
-          _depoistAmount = (msg.value * depositRatio) / 100;
-          depositReceipts[depositTimes] = depositReceipt(_depositTime, _depoistAmount);
-          depositTimes++;
-          lastDepositTime = _depositTime;
-          depositTotalAmount += _depoistAmount;
+            _depoistAmount = (msg.value * depositRatio) / 100;
+            depositReceipts[depositTimes] = depositReceipt(
+                _depositTime,
+                _depoistAmount
+            );
+            depositTimes++;
         }
         cash += msg.value - _depoistAmount;
     }
@@ -79,24 +84,41 @@ contract MyPiggyBank {
     }
 
     //withraw deposited amount method
-    function withdrawDeposit(uint256 amount) external onlyOwner {
-      require( lastDepositTime < block.timestamp," it's not time yet ");
-      require( depositTotalAmount > amount," not have engouht amount to withdraw ");       
-      payable(msg.sender).transfer(amount);
-      depositTotalAmount -= amount ;
+    function withdrawDeposit(uint256 amount) external onlyOwner checkwithraw {
+        withdrawStatus = true;
+        uint256 timeStamp = getBlockTime();
+        totalAmount = availableAmount(timeStamp);
+        require(totalAmount > amount, " not have engouht amount to withdraw ");
+        payable(msg.sender).transfer(amount);
+        totalAmount -= amount;
         for (uint256 i = 0; i < depositTimes; i++) {
-            uint256 _depositAmount = depositReceipts[i].depositAmount;         
+            uint256 _depositAmount = depositReceipts[i].depositAmount;
             if (_depositAmount <= amount) {
                 depositReceipts[i].depositAmount = 0;
                 amount -= _depositAmount;
             } else {
                 depositReceipts[i].depositAmount -= amount;
-                break ;
+                withdrawStatus = false;
+                break;
             }
         }
     }
 
-    function getBlockTime() public view  returns(uint256) {
-      return block.timestamp;
+    function availableAmount(
+        uint256 timeStamp
+    ) public view returns (uint256 amount) {
+        if (timeStamp == 0) {
+            timeStamp = block.timestamp;
+        }
+        for (uint256 i = 0; i < depositTimes; i++) {
+            uint256 _depositTime = depositReceipts[i].depositTime;
+            if (_depositTime <= timeStamp) {
+                amount += depositReceipts[i].depositAmount;
+            }
+        }
+    }
+
+    function getBlockTime() public view returns (uint256) {
+        return block.timestamp;
     }
 }
